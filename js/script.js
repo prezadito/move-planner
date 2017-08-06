fcf5b2cc5a87bf9aba2180abb4890450c717cca2
Aditya Niloi <aditya@incl.us>
2017-08-05 20:52:46 -0400

    formatting

--- js/script.js
+++ js/script.js
@@ -19,26 +19,26 @@ function loadData() {
     $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '&key=AIzaSyAW-4T5bpS-_wSsv8DwAMphCpdq-G4pRe8">');
 
     // Update greeting
-    $greeting.text('So you want to live at ' + address+ '?');
-    
+    $greeting.text('So you want to live at ' + address + '?');
+
     // Load New York Times articles
     var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + "&sort=newest&api-key=831032deb5a94bb6923a338fb8c201fe";
-    
-    $.getJSON(nytUrl, function( data ) {
-        $nytHeaderElem.text('New York Times Articles About ' + city);
 
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
-        };
-    })
-    .done(function() {
-        console.log("Loading NYT articles was a Success!");
+    $.getJSON(nytUrl, function(data) {
+            $nytHeaderElem.text('New York Times Articles About ' + city);
+
+            articles = data.response.docs;
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+                $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+            };
         })
-    .fail(function() {
-        $nytHeaderElem.text('No New York Times Articles About ' + city);
-    });
+        .done(function() {
+            console.log("Loading NYT articles was a Success!");
+        })
+        .fail(function() {
+            $nytHeaderElem.text('No New York Times Articles About ' + city);
+        });
 
 
     // Wikipedia AJAX requests
@@ -52,7 +52,7 @@ function loadData() {
     $.ajax(wikiUrl, {
         dataType: "jsonp",
         // jsonp: "callback",
-        success: function ( response ) {
+        success: function(response) {
             var articleList = response[1];
 
             for (var i = 0; i < articleList.length; i++) {
@@ -74,4 +74,3 @@ $('#form-container').submit(loadData);
 
 
 // UI Event handlers with Materialize
-
